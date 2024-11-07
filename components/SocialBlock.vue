3160c8f69719cab54c843f9a517e1edb33cf8dd6
alex-bulanov.mail <alex-bulanov.mail@yandex.ru>
2024-11-07 22:06:27 +0300

    Добавлен компонент соц. сетей

---
+++ components/SocialBlock.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+interface Props {}
+
+defineProps<Props>()
+</script>
+
+<template>
+	<div class="social-block">
+		<div class="social-block__wrapper">
+			<div class="social-block__item">
+				<a class="social-block__link" href="#!">
+					<NuxtIcon class="social-block__icon" name="discord" filled />
+				</a>
+			</div>
+			<div class="social-block__item">
+				<a class="social-block__link" href="#!">
+					<NuxtIcon class="social-block__icon" name="facebook" filled />
+				</a>
+			</div>
+			<div class="social-block__item">
+				<a class="social-block__link" href="#!">
+					<NuxtIcon class="social-block__icon" name="dribbble" filled />
+				</a>
+			</div>
+			<div class="social-block__item">
+				<a class="social-block__link" href="#!">
+					<NuxtIcon class="social-block__icon" name="instagram" filled />
+				</a>
+			</div>
+			<div class="social-block__item">
+				<a class="social-block__link" href="#!">
+					<NuxtIcon class="social-block__icon" name="behance" filled />
+				</a>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss">
+.social-block {
+	&__wrapper {
+		display: flex;
+
+		& > * + * {
+			margin-left: 13px;
+		}
+	}
+
+	&__link {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 36px;
+		height: 36px;
+	}
+
+	&__icon.nuxt-icon {
+		svg {
+			margin: 0;
+			width: 36px;
+			height: 36px;
+		}
+	}
+}
+</style>
