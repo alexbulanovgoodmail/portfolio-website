0d0553d5a8be9f3e73558dcab95dda4cd0414078
alex-bulanov.mail <alex-bulanov.mail@yandex.ru>
2024-11-07 22:04:45 +0300

    Обновлен конфиг. Добавлен модуль иконок

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2024-04-03',
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/eslint-module', '@nuxtjs/stylelint-module'],
+	modules: ['@nuxtjs/eslint-module', '@nuxtjs/stylelint-module', 'nuxt-icons'],
 
 	css: ['normalize.css', '~/assets/scss/main.scss'],
 
